35103907a8706fc4469261461bbda815e1c96156
Pugalenthis <aravindpugal505@gmail.com>
2022-10-18 11:59:16 +0530

    crud done

--- src/components/IssueList.js
+++ src/components/IssueList.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import React from "react";
 import { Link } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
-import { api } from "./api";
+import { api } from "./Api";
 import Issue from "./Issue";
 
 const IssueList = () => {
@@ -13,12 +13,16 @@ const IssueList = () => {
   return (
     <div>
       <div className="container mx-auto p-5">
-        <div className="grid grid-cols-2 gap-5">
-          {loading
-            ? "loading"
-            : data.map((item) => (
-                <Issue key={item.id} item={item} reFetch={reFetch} />
-              ))}
+        <div className="grid grid-cols-2 gap-5 gap-y-10">
+          {loading ? (
+            <div class="absolute inset-2/4 text-xl text-purple-500">
+              Loading...
+            </div>
+          ) : (
+            data.map((item) => (
+              <Issue key={item.id} item={item} reFetch={reFetch} />
+            ))
+          )}
         </div>
       </div>
     </div>
--- src/pages/EditIssue.js
+++ src/pages/EditIssue.js
@@ -1,25 +1,40 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { api } from "../components/api";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { api } from "../components/Api";
 
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
+import useFetch from "../hooks/useFetch";
 
 const EditIssue = () => {
+  const { id } = useParams();
   const handleChange = (e) => {
     setFormData((prev) => ({ ...prev, [e.target.id]: e.target.value }));
   };
-  const [formData, setFormData] = useState({
-    title: undefined,
-    description: undefined,
-  });
+
+  const { data, loading, error, reFetch } = useFetch(`${api}/${id}`);
+  console.log(data);
+
+  const [formData, setFormData] = useState(data);
+
+  console.log(formData);
+
+  useEffect(() => {
+    setFormData(data);
+  }, [data]);
 
   const navigate = useNavigate();
 
+  const [updateLoader, setUpdateLoader] = useState(false);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     try {
-      const res = await axios.put(`${api}`, formData);
+      setUpdateLoader(true);
+      const res = await axios.put(`${api}/${id}`, formData);
+      setUpdateLoader(false);
       console.log(res);
       navigate("/");
     } catch (error) {
@@ -29,42 +44,54 @@ const EditIssue = () => {
 
   return (
     <div>
-      <Navbar />
-      <section class="text-gray-600 body-font relative">
-        <form onSubmit={handleSubmit}>
-          <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-            <div class="lg:w-3/3 md:w-3/3 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-              <div class="relative mb-4">
-                <label for="name" class="leading-7 text-sm text-gray-600">
-                  Title
-                </label>
-                <input
-                  type="text"
-                  id="title"
-                  onChange={handleChange}
-                  name="name"
-                  class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
+      {loading || updateLoader ? (
+        <div class="absolute inset-2/4 text-xl text-purple-500">
+          Loading....
+        </div>
+      ) : (
+        <div>
+          <Navbar />
+          <section class="text-gray-600 body-font relative">
+            <form onSubmit={handleSubmit}>
+              <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+                <div class="lg:w-3/3 md:w-3/3 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                  <div class="relative mb-4">
+                    <label for="name" class="leading-7 text-sm text-gray-600">
+                      Title
+                    </label>
+                    <input
+                      type="text"
+                      id="title"
+                      value={formData.title}
+                      onChange={handleChange}
+                      name="name"
+                      class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                  </div>
+                  <div class="relative mb-4">
+                    <label
+                      for="message"
+                      class="leading-7 text-sm text-gray-600"
+                    >
+                      Description
+                    </label>
+                    <textarea
+                      id="description"
+                      value={formData.description}
+                      onChange={handleChange}
+                      name="message"
+                      class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                    ></textarea>
+                  </div>
+                  <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                    Update Issue
+                  </button>
+                </div>
               </div>
-
-              <div class="relative mb-4">
-                <label for="message" class="leading-7 text-sm text-gray-600">
-                  Description
-                </label>
-                <textarea
-                  id="description"
-                  onChange={handleChange}
-                  name="message"
-                  class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                ></textarea>
-              </div>
-              <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                Button
-              </button>
-            </div>
-          </div>
-        </form>
-      </section>
+            </form>
+          </section>
+        </div>
+      )}
     </div>
   );
 };
